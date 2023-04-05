637e255e8bc63c43dcc04e78dafe48438eed2e40
Paul <128988017+PaulAMoretto@users.noreply.github.com>
2023-04-04 22:16:10 -0500

    playing around with JS, need to make it functional

--- new.js
+++ new.js
@@ -1,2 +1,11 @@
-var msg = "Hello World"
-console.log(msg)
+var span = document.getElementById('span')
+function time(){
+    var d = new Date ();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    span.textContent =
+        ("0" +h).substring(-2) + ":"+("0"+m).substring(-2)+ ":" + ("0" + s ).substring(-2);       
+}
+setInterval(time, 1000);
+<span id = "span"></span>
